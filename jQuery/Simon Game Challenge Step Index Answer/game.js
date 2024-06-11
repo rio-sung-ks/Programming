c31c8edc8d43719afdc1cbe203276d832e426472
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2024-06-11 18:17:57 +0900

    simon game

--- jQuery/Simon Game Challenge Step Index Answer/game.js
+++ jQuery/Simon Game Challenge Step Index Answer/game.js
@@ -2,13 +2,29 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var level = 0;
+
+$(document).on("keypress",function(){  
+  nextSequence(); 
+  // alert("here too!")
+});
+
 $(".btn").click(function(){
 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
-
+    animatePress(userChosenColour);
 })
+
+
+function animatePress(currentColour){
+
+  $("#"+currentColour).addClass("pressed");  
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  }, 100);
+}
 
 
 function nextSequence(){
@@ -17,13 +33,12 @@ function nextSequence(){
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour);
+    playSound(randomChosenColour);  
+    level = level + 1;
+    $("h1").text("Level "+level);
 }
 
 function playSound(name){
     var audio = new Audio("./sound/" + name + '.mp3');
     audio.play();        
 }
-
-
-
--- jQuery/index.js
+++
@@ -1,6 +0,0 @@
-
-$(document).click(function(){
-    $("h1").slideUp().slideDown().animate({opacity:0.5})
-});
-
-
