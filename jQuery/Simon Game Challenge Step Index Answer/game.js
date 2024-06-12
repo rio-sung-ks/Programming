a4f797307d853af635d06d7f3bdc7efaf672302a
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2024-06-12 18:20:28 +0900

    game

--- jQuery/Simon Game Challenge Step Index Answer/game.js
+++ jQuery/Simon Game Challenge Step Index Answer/game.js
@@ -2,22 +2,28 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 // var index = 0;
+var started = false;
 var level = 0;
 
 $(document).on("keypress",function(){  
-  nextSequence(); 
+  // nextSequence(); 
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
 
 $(".btn").click(function(){
 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+
     playSound(userChosenColour);
     animatePress(userChosenColour);
-    checkAnswer(index)
+    checkAnswer(userClickedPattern.length-1);
 
 })
-
 
 function animatePress(currentColour){
 
@@ -30,13 +36,15 @@ function animatePress(currentColour){
 
 function nextSequence(){
 
+    userClickedPattern = [];
+    level = level + 1;
+    $("h1").text("Level "+level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);  
-    level = level + 1;
-    $("h1").text("Level "+level);
+
 }
 
 function playSound(name){
@@ -45,11 +53,21 @@ function playSound(name){
 }
 
 
-function checkAnswer(currentLevel){
-  
-//   2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
-// e.g. If the user has pressed red, green, red, yellow, the index of the last answer is 3.
 
-  var index = 
-  
+function checkAnswer(currentLevel){
+
+  if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+    console.log("success");
+    if(gamePattern.length === userClickedPattern.length){
+      setTimeout( function(){
+        nextSequence();
+        
+      }, 1000);
+      
+    }
+  }
+
+  else{
+    console.log("wrong");
+  }  
 }
