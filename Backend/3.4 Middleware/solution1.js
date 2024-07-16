85d98488d8885458e2c2041cc9bd1a126352ff1c
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2024-07-16 17:57:49 +0900

    morgan

--- Backend/3.4 Middleware/solution1.js
+++ Backend/3.4 Middleware/solution1.js
@@ -8,15 +8,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
 res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-});
+app.post()
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
