0062a5c47ea98ea8d9d7f42d8e439c5b476c9acf
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2024-07-22 18:48:15 +0900

    ejs

--- Backend/3.4 Middleware 3/index3.js
+++ Backend/3.4 Middleware 3/index3.js
@@ -1,13 +1,25 @@
+
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = 3000;
 
+function logger(req, res, next){
+  console.log("request method : ", req.method)
+  console.log("request url : ", req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("hello");
 });
+app.post("/submit",(req,res)=>{
+  cconsole.log("request method : ", req.method)
+  console.log("request url : ", req.url);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
---
+++ Backend/3.5 Secrets Project/index.js
@@ -0,0 +1,43 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express"
+// import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+var password = "";
+var check = false;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+function passwordCheck(req,res,next){
+    // console.log(req.body["password"])
+    password = req.body["password"];    
+    next();
+}
+
+app.use(passwordCheck);
+app.post("/check",(req,res)=>{
+    if(password === "love"){  
+        password = "success"
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else{
+        // password = "fail"
+        res.sendFile(__dirname + "/public/index.html");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
