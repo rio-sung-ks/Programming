8540862cce085d7666df69451861c4151e49b145
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2024-08-19 16:57:30 +0900

    Ok

--- React.js/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.createRoot(
-  <div><h1>testeeessse</h1></div>
-  ,document.getElementById("root"));
--- React.js/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>App.jsx 돌아가는 중입니다!</div>;
-}
-
-export default App;
