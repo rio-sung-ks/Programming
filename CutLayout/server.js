15f91ba072f8fd1b2c21844ec5b39b85b625807c
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2024-08-29 18:15:40 +0900

    df

--- CutLayout/server.js
+++ CutLayout/server.js
@@ -8,13 +8,27 @@ const port = 3000;
 app.use(cors());
 
 app.get('/api/data', async (req, res) => {
-    try {
-        var factory = req.param('Factory');
-        // var value = req.param('Value');
+    try {   
+        var factory = req.params.Factory;
         const data = await fetchData();  // 데이터를 가져옴
         let result = [];
-        for (const item of data) {
-            result.push({Name: item[0], Value: item[1]});
+        for (const item of factory) {
+            result.push({Factory: item[0], Value: item[1]});
+        }
+        res.json(result);  // 데이터를 JSON 형식으로 클라이언트에 반환
+        console.log(result)
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching data" });
+    }
+});
+
+app.get('/api/', async (req, res) => {
+    try {   
+        var factory = req.params.Factory;
+        const data = await fetchData();  // 데이터를 가져옴
+        let result = [];
+        for (const item of factory) {
+            result.push({Factory: item[0], Value: item[1]});
         }
         res.json(result);  // 데이터를 JSON 형식으로 클라이언트에 반환
         console.log(result)
